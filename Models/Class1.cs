c8fcd8dd4cca6c3012643a4de53cc9550ba61bed
Joshua Sam C Picato <126132355+JoshuaSPicato@users.noreply.github.com>
2023-03-03 02:20:31 -0800

    Add files via upload

---
+++ Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿
+public class UserModel 
+{
+    public int ID { get; set; }
+    public string UserName { get; set;  }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Password { get; set; }
+    internal class Class1
+    {
+    }
+}
---
+++ Models/ListPostModel.cs
@@ -0,0 +1,12 @@
+﻿public class ListPostModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public Datetime DateCrreated { get; set; }
+    public string Username { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    
+    }
+
