7a76692f3f28aa50b5f6a4f83b7526b0bbc9279a
Joshua Sam C Picato <126132355+JoshuaSPicato@users.noreply.github.com>
2023-03-03 01:04:55 -0800

    Add files via upload

---
+++ PicatoBlogDB/dbo/Tables/Posts_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Posts]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserId] INT NOT NULL, 
+    [Title] NVARCHAR(150) NOT NULL, 
+    [Body] TEXT NOT NULL, 
+    [DateCreated] DATETIME2 NOT NULL 
+)
